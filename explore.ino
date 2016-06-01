f0444f7ed6750e6f8ebf7830019c555e49e5d437
geoffhall2013 <geoffhall@rogers.com>
2016-05-31 21:59:51 -0400

    basics are now working

--- explore.ino
+++ explore.ino
@@ -13,8 +13,8 @@
 #define TURN_RIGHT_S 6
 
 // Ultrasonic
-#define echoPin 17 // Echo Pin
-#define trigPin 18 // Trigger Pin
+#define echoPin 17 // A3 Echo Pin
+#define trigPin 18 // A4 Trigger Pin
 NewPing sonar(trigPin, echoPin);
 int distance;
 
@@ -29,6 +29,7 @@ MotorController mc(dirAPin, pwmAPin, dirBPin, pwmBPin, 1, 1);
 uint8_t fsm_state = STOP_S;
 int turn_cnt;
 
+#define runEvery(t) for (static typeof(t) _lasttime;(typeof(t))((typeof(t))millis() - _lasttime) > (t);_lasttime += (t))
 void setup(){
   Serial.begin(115200);
   // software version
@@ -37,48 +38,50 @@ void setup(){
 }
 
 void loop(){
-  //state machine
-  switch (fsm_state)
-  {
-    case STOP_S:  //Statements to execute
-      // stop motors
-      mc.stopMoving();
-      // check distance ahead
-        distance = sonar.ping();
-      // if clear - go forward
-      if(distance > 1200) {
-        fsm_state = FORWARD_S;
-      }
-      // else stay in stop
-      break;
-    case FORWARD_S:   //Statements to execute
-      // check distance ahead
-      distance = sonar.ping();
-      // if clear - go forward
-      if(distance > 1200) {
-         mc.move(-255,-255);
-      } else {  // else turn left
-      fsm_state = LEFT_S;
-      }
-      break;
-    case LEFT_S:
-      // set turning time
-      turn_cnt = 100;
-      // go to turn left
-      fsm_state = TURN_LEFT_S;
-      break;
-    case TURN_LEFT_S:
-      // decrement turn counter
-      turn_cnt--;
-      // if == zero - then go forward
-      if(turn_cnt <= 0) {
-        fsm_state = FORWARD_S;
-      }
-      // else keep turning
-      break;
-    default:
-      break;
-  }
 
-  delay(1000);              // wait for a second
+  runEvery(50) {
+      //state machine
+      switch (fsm_state)
+      {
+        case STOP_S:  //Statements to execute
+          // stop motors
+          mc.stopMoving();
+          // check distance ahead
+          distance = sonar.ping();
+          // if clear - go forward
+          if(distance > 1200) {
+              fsm_state = FORWARD_S;
+          }
+          // else stay in stop
+          break;
+        case FORWARD_S:   //Statements to execute
+          // check distance ahead
+          distance = sonar.ping();
+          // if clear - go forward
+          if(distance > 1200) {
+              mc.move(-255,-255);
+          } else {  // else turn left
+          fsm_state = LEFT_S;
+          }
+          break;
+        case LEFT_S:
+          // set turning time
+          turn_cnt = 10;
+          // go to turn left
+          fsm_state = TURN_LEFT_S;
+          break;
+       case TURN_LEFT_S:
+         // decrement turn counter
+         turn_cnt--;
+         // if == zero - then go forward
+         if(turn_cnt == 0) {
+             fsm_state = FORWARD_S;
+         } else { // keep turning
+            mc.move(255,55);
+         }
+         break;
+       default:
+         break;
+      }
+  }
 }
